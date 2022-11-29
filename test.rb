7fc720df4320bfe9b5b3fe13cadb120dcc168db8
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2022-11-29 09:15:49 -0400

    Add photo 16

---
+++ test.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+    def index
+      @users = User.all
+  
+      respond_to do |format|
+        format.html
+        format.json { render json: @users }
+      end
+    end
+  
+    def show
+      @user = User.find(params[:id])
+    end
+  
+    def new
+      @user = User.new
+    end
+
+    def edit  
+      @user = User.find(params[:id])  
+    end
+
+    def create  
+      @user = User.new(params[:user])  
+      if @user.save  
+        redirect_to @user, notice: 'User was successfully created.'  
+      else  
+        render action: "new"  
+      end  
+    end
+
+    def update  
+      @user = User.find(params[:id])  
+      if @user.update_attributes(params[:user])  
+        redirect_to @user, notice: 'User was successfully updated.'  
+      else  
+        render action: "edit"  
+      end  
+    end
+
+end
